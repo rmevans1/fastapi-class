c6ce487a8c6d731c878b43395e103024dd5ab597
Robert Evans <robertevans@robertevansmb.com>
2023-04-19 09:46:33 -0400

    Combine path and query parameters

--- main.py
+++ main.py
@@ -17,6 +17,10 @@ def index():
 def get_all_blogs(page=1, page_size: Optional[int] = None):
     return {'message': f'All {page_size} blogs on page {page}'}
 
+@app.get('/blog/{id}/comments/{comment_id}')
+def get_comment(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
+    return {'message': f'blog_id {id}, comment_id {comment_id}, valid {valid}, username {username}'}
+
 class BlogType(str, Enum):
     short = 'short'
     story = 'story'
