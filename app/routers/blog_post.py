f807e6e6ad38f38e1c670228c817a735d7194a88
Robert Evans <robertevans@robertevansmb.com>
2023-04-19 15:12:39 -0400

    Dependency intro

--- app/routers/blog_post.py
+++ app/routers/blog_post.py
@@ -36,3 +36,6 @@ def create_comment(blog: BlogModel, id: int,
         'version': v,
         'comment_id': comment_id
     }
+
+def required_functionality():
+    return {'message': 'Learning FastAPI is important'}
