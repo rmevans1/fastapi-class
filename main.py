ea2fa1c8f7d494f911f06e559b87921546862579
Robert Evans <robertevans@robertevansmb.com>
2023-04-19 10:11:56 -0400

    Add description through doc

--- main.py
+++ main.py
@@ -14,7 +14,9 @@ def index():
 
 @app.get(
     '/blog/all',
-    tags=['blog']
+    tags=['blog'],
+    summary='Retrieve all blogs',
+    description='This api call simulates fetching all blogs.'
 )
 def get_all_blogs(page=1, page_size: Optional[int] = None):
     return {'message': f'All {page_size} blogs on page {page}'}
@@ -24,6 +26,14 @@ def get_all_blogs(page=1, page_size: Optional[int] = None):
     tags=['blog', 'comment']
 )
 def get_comment(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
+    """
+    Simulates retrieving a comment of a blog
+
+    - **id** mandatory path parameter
+    - **comment_id** mandatory parameter
+    - **valid** optional query parameter
+    - **username** optional query parameter
+    """
     return {'message': f'blog_id {id}, comment_id {comment_id}, valid {valid}, username {username}'}
 
 class BlogType(str, Enum):
