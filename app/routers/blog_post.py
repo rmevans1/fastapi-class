7facaec93682e57ddabf52657f8a449ee879a880
Robert Evans <robertevans@robertevansmb.com>
2023-04-19 14:57:09 -0400

    Custom Subtype

--- app/routers/blog_post.py
+++ app/routers/blog_post.py
@@ -7,6 +7,10 @@ router = APIRouter(
     tags=['blog']
 )
 
+class Image(BaseModel):
+    url: str
+    alias: str
+
 class BlogModel(BaseModel):
     title: str
     content: str
@@ -14,6 +18,7 @@ class BlogModel(BaseModel):
     published: Optional[bool]
     tags: List[str] = []
     metadata: Dict[str, str] = {'key1': 'val1'}
+    image: Optional[Image] = None
 
 @router.post('/new/{id}')
 def create_blog(blog: BlogModel, id: int, version: int=1):
