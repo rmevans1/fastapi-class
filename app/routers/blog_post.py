854d7779a5687d8fd1fc22bf3f1d6963fc4d9c37
Robert Evans <robertevans@robertevansmb.com>
2023-04-19 11:11:48 -0400

    Simple model

--- app/routers/blog_post.py
+++ app/routers/blog_post.py
@@ -1,10 +1,17 @@
+from typing import Optional
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 router = APIRouter(
     prefix='/blog',
     tags=['blog']
 )
 
+class BlogModel(BaseModel):
+    title: str
+    content: str
+    published: Optional[bool]
+
 @router.post('/new')
-def create_blog():
-    pass
+def create_blog(blog: BlogModel):
+    return {'data': blog}
