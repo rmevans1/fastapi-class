e403007662bfc3ba84d855aae6c253797c490cb5
Robert Evans <robertevans@robertevansmb.com>
2023-04-20 11:52:22 -0400

    Remove password from user creation resposne

--- app/routers/user.py
+++ app/routers/user.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.db import db_user
 from app.db.database import get_db
-from app.schemas.schemas import UserBase
+from app.schemas.schemas import UserBase, UserDisplay
 
 router = APIRouter(
     prefix='/user',
@@ -10,7 +10,7 @@ router = APIRouter(
 )
 
 # Create User
-@router.post('/')
+@router.post('/', response_model=UserDisplay)
 def create_user(request: UserBase, db: Session = Depends(get_db)):
     return db_user.create_user(db, request)
 
