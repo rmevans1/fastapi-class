ced94181f39f3ff370960285860b843c772b5006
Robert Evans <robertevans@robertevansmb.com>
2023-04-20 10:08:26 -0400

    db_user

---
+++ app/db/db_user.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import Session
+
+from app.db.hash import Hash
+from app.db.models import DbUser
+from app.schemas.schemas import UserBase
+
+
+def create_user(db: Session, request: UserBase):
+    new_user = DbUser(
+        username=request.username,
+        email=request.email,
+        password=Hash(request.password)
+    )
