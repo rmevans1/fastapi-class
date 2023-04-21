ef7c1729ef01e2bfcb51badd5e7552f23ea80ed5
Robert Evans <robertevans@robertevansmb.com>
2023-04-21 11:02:12 -0400

    Article Display

--- app/schemas/schemas.py
+++ app/schemas/schemas.py
@@ -21,8 +21,22 @@ class UserDisplay(BaseModel):
     class Config():
         orm_mode = True
 
+class User(BaseModel):
+    id: int
+    username: str
+    class Config():
+        orm_mode = True
+
 class ArticleBase(BaseModel):
     title: str
     content: str
     published: bool
     creator_id: int
+
+class ArticleDisplay(BaseModel):
+    title: str
+    content: str
+    published: bool
+    user: User
+    class Config():
+        orm_mode = True
