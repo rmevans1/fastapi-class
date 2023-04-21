d5bd64a74a10dc35f7b04d260095269c6d656304
Robert Evans <robertevans@robertevansmb.com>
2023-04-21 11:06:58 -0400

    Create article

--- app/db/db_article.py
+++ app/db/db_article.py
@@ -3,7 +3,16 @@ from app.db.models import DbArticle
 from app.schemas.schemas import ArticleBase
 
 def create_article(db: Session, request: ArticleBase):
-    pass
+    new_article = DbArticle(
+        title=request.title,
+        content=request.content,
+        published=request.published,
+        user_id=request.creator_id
+    )
+    db.add(new_article)
+    db.commit()
+    db.refresh(new_article)
+    return new_article
 
 def get_article(db: Session, id: int):
     article = db.query(DbArticle).filter(DbArticle.id == id).first()
