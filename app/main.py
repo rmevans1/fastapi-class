551b0f2017a1df54d5cab6b80f7e41b310d57389
Robert Evans <robertevans@robertevansmb.com>
2023-04-21 15:37:48 -0400

    Handle exception

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from app.exceptions import StoryException
 from app.routers import blog_get, blog_post, user, article
 from app.db import models
 from app.db.database import engine
@@ -12,6 +14,13 @@ app.include_router(blog_post.router)
 @app.get('/hello')
 def index():
     return {'message': 'Hello World!'}
+
+@app.exception_handler(StoryException)
+def story_exception_handler(request: Request, exc: StoryException):
+    return JSONResponse(
+        status_code=418,
+        content={'detail': exc.name}
+    )
 
 models.Base.metadata.create_all(engine)
 
