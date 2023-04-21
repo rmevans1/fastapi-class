a592af73b1a661c2c1aae5d55bd62c8065291d1d
Robert Evans <robertevans@robertevansmb.com>
2023-04-21 11:12:36 -0400

    Article router

---
+++ app/routers/article.py
@@ -0,0 +1,21 @@
+from typing import List
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from app.db import db_article
+from app.db.database import get_db
+from app.schemas.schemas import ArticleBase, ArticleDisplay
+
+router = APIRouter(
+    prefix='/article',
+    tags=['article']
+)
+
+# Create Article
+@router.post('/', response_model=ArticleDisplay)
+def create_article(request: ArticleBase, db: Session = Depends(get_db)):
+    return db_article.create_article(db, ArticleBase)
+
+# Get Article
+@router.get('/{id}', response_model=ArticleDisplay)
+def get_article(id: int, db: Session = Depends(get_db)):
+    return db_article.get_article(db, id)
