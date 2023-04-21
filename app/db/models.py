077e17edd6111e9cc68475863376613300eb6556
Robert Evans <robertevans@robertevansmb.com>
2023-04-21 10:56:55 -0400

    Relationship model

--- app/db/models.py
+++ app/db/models.py
@@ -1,6 +1,7 @@
+from sqlalchemy.orm import relationship
 from app.db.database import Base
-from sqlalchemy import Column
-from sqlalchemy.sql.sqltypes import Integer, String
+from sqlalchemy import Column, ForeignKey
+from sqlalchemy.sql.sqltypes import Integer, String, Boolean
 
 
 class DbUser(Base):
@@ -9,3 +10,13 @@ class DbUser(Base):
     username = Column(String(255))
     email = Column(String(255))
     password = Column(String(255))
+    items = relationship('DbArticle', back_populates='user')
+
+class DbArticle(Base):
+    __tablename__='articles'
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(255))
+    content = Column(String(255))
+    published = Column(Boolean)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship("DbUser", back_populates='items')
