3eeb9e024d683bf1f821d38dd1c8c3e327243ae7
Robert Evans <robertevans@robertevansmb.com>
2023-04-24 11:02:56 -0400

    Return html response

--- app/routers/product.py
+++ app/routers/product.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from fastapi.responses import Response
+from fastapi.responses import Response, HTMLResponse
 
 router = APIRouter(
     prefix='/product',
@@ -12,3 +12,22 @@ def get_all_products():
     #return products
     data = " ".join(products)
     return Response(content=data, media_type="text/plain")
+
+@router.get('/{id}')
+def get_product(id: int):
+    product = products[id]
+    out = f"""
+    <head>
+        <style>
+            .product {{
+                width: 500px;
+                height: 30px;
+                border: 2px inset green;
+                background-color: lightblue;
+                text-align: center;
+            }}
+        </style>
+    </head>
+    <div class="product">{product}</div>
+    """
+    return HTMLResponse(content=out, media_type="text/html")
